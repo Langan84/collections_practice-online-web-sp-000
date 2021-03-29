c5246f6164fd9c6b2c885e7aa4e8a868987610d9
langan.kg Langan <langan.kg@gmail.com>
2021-03-29 12:39:22 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -46,7 +46,7 @@ def sum_array(int)
 end
 
 def add_s(array)
-  array.each_with_index.collect{|element, index|}
+  array.each_with_index.collect{|element,index|}
   if index == 1
     return element
   else
